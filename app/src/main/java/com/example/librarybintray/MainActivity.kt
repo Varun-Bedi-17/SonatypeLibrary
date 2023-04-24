7f2820cb9f0e9a84ab0e4cde1985b90c044b830d
Varun-Bedi-17 <varunbedi02@gmail.com>
2023-04-24 15:38:48 +0530

    First Commmit

---
+++ app/src/main/java/com/example/librarybintray/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.librarybintray
+
+import android.icu.lang.UCharacter.LineBreak
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.library.MyLibrary
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
